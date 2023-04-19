91a812e8cb12cc4a457b5dca2760691de96bcf3b
P M Mohamed Jaseem Jabir <pmmohamed.j@thoughtworks.com>
2023-04-19 12:23:59 +0530

    [Jaseem|Roja] Refactor. ignore command should recalculate the styles in the active document upon ignoring a signal

--- src/commands/removeSignal.ts
+++ src/commands/removeSignal.ts
@@ -1,11 +1,16 @@
 import * as vscode from "vscode";
 import Signal from "../models/Signal";
+import addStylesToCodeTriggers from "../presentation/codeTriggerStyles";
 
-export default async (signal: Signal,context:vscode.ExtensionContext) => {
-    const currentSignals: Signal[] =
-      (await context.workspaceState.get("signals")) || [];
-    const updatedSignals = currentSignals.filter(
-      (curSignal) => curSignal.name !== signal.name
-    );
-    await context.workspaceState.update("signals", updatedSignals);
+export default async (signal: Signal, context: vscode.ExtensionContext) => {
+  const currentSignals: Signal[] =
+    (await context.workspaceState.get("signals")) || [];
+  const updatedSignals = currentSignals.filter(
+    (curSignal) => curSignal.name !== signal.name
+  );
+  await context.workspaceState.update("signals", updatedSignals);
+  const editor = vscode.window.activeTextEditor;
+  if (editor) {
+    addStylesToCodeTriggers(editor, context);
   }
+};
