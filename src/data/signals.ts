feec20a386664d47636bac4670409f1588246308
P M Mohamed Jaseem Jabir <pmmohamed.j@thoughtworks.com>
2023-04-20 10:15:39 +0530

    [Jaseem|Roja] Add. codeTriggers for signal 2

--- src/data/signals.ts
+++ src/data/signals.ts
@@ -6,13 +6,25 @@ export default [
     "Prefer testing user behaviour on UI components rather than testing implementation details, such as the presence of elements in the UI at a certain moment in time.",
     "https://docs.google.com/presentation/d/15Bw1qwvfuJ3bswOUS0HvNJbR2EVmHLtPk_cWNWuLVf0/edit#slide=id.g1c385a94172_0_0",
     [],
-    [/@testing-library\/react/g, /\benzyme\b/gi]
+    [/@testing-library\/react/g, /\benzyme\b/g]
   ),
   new Signal(
     "Native state management for simple apps",
     "Many of the front end frameworks come with their own support when it comes to application level state management solutions. It is preferable to use these native solutions instead of using third party libraries.",
     "https://docs.google.com/presentation/d/15Bw1qwvfuJ3bswOUS0HvNJbR2EVmHLtPk_cWNWuLVf0/edit#slide=id.g1c303425273_0_56",
     ["reducer"],
-    []
+    [
+      /\buseContext\b/g,
+      /\bcreateStore\b/g,
+      /\breact-redux\b/g,
+      /\bdispatch\b/g,
+      /\bconnect\b/g,
+      /\bProvider\b/g,
+      /\bcreateReducer\b/g,
+      /\bcreateAction\b/g,
+      /\bcreateContext\b/g,
+      /\bconfigureStore\b/g,
+      /@reduxjs\/toolkit/g,
+    ]
   ),
 ];
