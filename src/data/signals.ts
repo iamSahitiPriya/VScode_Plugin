e2fbbc54f121826424a896bb3d0b63db43d75be7
P M Mohamed Jaseem Jabir <pmmohamed.j@thoughtworks.com>
2023-04-24 17:35:07 +0530

    [Jaseem] Add. signal 4 triggers

--- src/data/signals.ts
+++ src/data/signals.ts
@@ -6,7 +6,7 @@ export default [
     "Prefer testing user behaviour on UI components rather than testing implementation details, such as the presence of elements in the UI at a certain moment in time.",
     "https://docs.google.com/presentation/d/15Bw1qwvfuJ3bswOUS0HvNJbR2EVmHLtPk_cWNWuLVf0/edit#slide=id.g1c385a94172_0_0",
     [],
-    [/@testing-library\/react/g, /\benzyme\b/g]
+    [/@testing-library\/(\w+(-\w+)?)/g, /\benzyme\b/g]
   ),
   new Signal(
     "Native state management for simple apps",
@@ -28,18 +28,31 @@ export default [
     ]
   ),
   new Signal(
+    "Component Libraries for consistency and faster development",
+    "A component library is a collection of pre-built reusable UI elements (such as buttons, forms, and layout elements) that can be used to build user interfaces. These libraries can be easily incorporated into applications, saving developers time and ensuring consistency in design and functionality. You can either adopt the component library as is or adapt the component libraries to adhere to your design language. Some popular UI component libraries include Bootstrap, Material UI, Ant Design and Chakra UI",
+    "https://docs.google.com/presentation/d/15Bw1qwvfuJ3bswOUS0HvNJbR2EVmHLtPk_cWNWuLVf0/edit#slide=id.g1c303425273_0_42",
+    [],
+    [
+      /@emotion\/\w+/g,
+      /@chakra-ui\/\w+/g,
+      /@fluentui\/\w+/g,
+      /@fluentui\/react\/\w+\/\w+/g,
+      /@mui\/material/g,
+    ]
+  ),
+  new Signal(
     "CSS in JS to manage CSS at scale",
     "Scope CSS to your UI components to scale CSS in large code-bases and avoid naming collisions.",
     "https://docs.google.com/presentation/d/15Bw1qwvfuJ3bswOUS0HvNJbR2EVmHLtPk_cWNWuLVf0/edit#slide=id.g1c303425273_0_14",
     ["module.css", ".scss"],
     [
       /\bstyled-components\b/g,
-      /@emotion\/styled/g,
-      /@emotion\/react/g,
+      /@emotion\/\w+/g,
       /\breact-jss\b/g,
       /\bstyled-system\b/g,
       /\bstitches\b/g,
       /\buseTheme\b/g,
+      /\bemotion\b/g,
       /\bstyled\.\w+/g,
     ]
   ),
